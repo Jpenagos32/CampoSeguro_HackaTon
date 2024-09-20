3f930cc844ff168e27042f1df489a2ca20299999
jpenagos32 <jpenagosdev@gmail.com>
2024-09-20 12:50:05 -0500

    Arreglar cors

--- CampoSeguroBack/app.js
+++ CampoSeguroBack/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express()
 
 const corsOptions = {
-  origin: 'https://campo-seguro-hacka-ton.vercel.app/',
+  origin: 'https://campo-seguro-hacka-ton.vercel.app',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
@@ -16,6 +16,19 @@ app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  )
+  next()
+})
 
 app.use('/notification', notificationRouter)
 
